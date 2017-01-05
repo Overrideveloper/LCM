20eac5e341b4dd1ebd7b8dd517877c4e8c4e6d49
Banso D. Wisdom <bansowisdom@gmail.com>
2017-01-05 13:08:59 +0100

    Adding time and memory usage profiling. ✌✌

--- LCM.py
+++ LCM.py
@@ -1,6 +1,10 @@
 #Owner: Overrideveloper
 #Description: LCM Program 
 
+from memory_profiler import memory_usage
+from datetime import datetime
+startTime = datetime.now()
+
 #Creating a function that uses two parameters 
 def GCD(x, y):
 
@@ -32,3 +36,10 @@ print ("The Lowest Common Multiple of the five numbers is: ")
 
 print (output)
         
+print ("The execution time is ",(datetime.now()- startTime))
+
+mem_usage = memory_usage(LCM)
+
+print('Memory usage (in chunks of .1 seconds): %s'% mem_usage)
+
+print('Maximum memory usage: %s'% max(mem_usage))
